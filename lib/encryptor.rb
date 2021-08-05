0de8c40be2878293d2277b69345c44bbe6f4122f
Tanner Dale <tanner@tannerdale.com>
2021-08-05 17:07:05 -0500

    Add full encryptor class" "

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -5,8 +5,10 @@ class Encryptor
   include KeyMaker
   attr_reader :message, :key, :date, :offsets
 
-  def initialize(message, key=make_key_string, date=format_today)
-    @message = message
+  ALPHABET = ('a'..'z').to_a << ' '
+
+  def initialize(message, key, date)
+    @message = message.downcase
     @date = date
     @key = key
     @offsets = Offset.offsets(@key, @date)
@@ -15,5 +17,36 @@ class Encryptor
   def format_today
     date = Time.now
     date.strftime("%d%m") + date.strftime("%Y")[2..]
+  end
+
+  def encrypted_message
+    @message.chars.each_with_index.map do |letter, i|
+      if ALPHABET.include?(letter)
+        ALPHABET[next_letter(letter, @offsets[i % 4])]
+      else
+        letter
+      end
+    end.join
+  end
+
+  def next_letter(letter, shift)
+    (ALPHABET.index(letter) + shift) % ALPHABET.size
+  end
+
+  def format_encryption
+    {
+      encryption: encrypted_message,
+      key: @key,
+      date: @date
+    }
+  end
+
+  def self.format_today
+    date = Time.now
+    date.strftime("%d%m") + date.strftime("%Y")[2..]
+  end
+
+  def self.encrypt(message, key=make_key_string, date=format_today)
+    Encryptor.new(message, key, date).format_encryption
   end
 end
