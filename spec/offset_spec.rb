f135bbcb1debd1b49cf12ebc4536f18b032ac891
Tanner Dale <tanner@tannerdale.com>
2021-08-05 14:44:24 -0500

    rename methods, add offset creation tests

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -35,8 +35,32 @@ RSpec.describe Offset do
       expect(offset.make_key_string).to eq("00256")
     end
 
-    it 'can format keys' do
-      expect(offset.format_keys("02715")).to eq(%w(02 27 71 15))
+    it 'can seperate keys' do
+      expect(offset.seperated_keys("02715")).to eq(%w(02 27 71 15))
     end
+
+    it 'can format keys' do
+      expect(offset.format_keys("02715")).to eq({a: 2, b: 27, c: 71, d: 15})
+    end
+  end
+
+  context 'offset making' do
+    offset = Offset.new("02715", "040895")
+
+    it 'can make offset hash' do
+      expect(offset.make_offsets).to eq({a: 3, b: 27, c: 73, d: 20})
+    end
+
+    it 'can make date modfiers hash' do
+      expect(offset.date_modifiers). to eq({a: 1, b: 0, c: 2, d: 5})
+    end
+
+    it 'can calculate date modifiers' do
+      expect(offset.date_modifiers_array). to eq([1, 0, 2, 5])
+    end
+  end
+
+  it 'can make offsets' do
+    expect(Offset.offsets("02715", "040895")).to eq({a: 3, b: 27, c: 73, d: 20})
   end
 end
