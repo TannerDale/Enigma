1c1a9bb529bbd9ef02ae20009688512875542cd3
Tanner Dale <tanner@tannerdale.com>
2021-08-05 14:45:04 -0500

    Add make offset hash test

--- lib/key_maker.rb
+++ lib/key_maker.rb
@@ -1,5 +1,12 @@
 module KeyMaker
   def format_keys(key=make_key_string)
+    letter_keys = [:a, :b, :c, :d]
+    seperated_keys(key).each_with_index.map do |key, i|
+      [letter_keys[i], key.to_i]
+    end.to_h
+  end
+
+  def seperated_keys(key)
     key.chars.each_cons(2).map do |key|
       key.join
     end
