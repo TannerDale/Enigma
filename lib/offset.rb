7f799fb98a8c8873c8976f59cbec5316f725ad43
Tanner Dale <tanner@tannerdale.com>
2021-08-05 14:45:19 -0500

    Add offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -14,4 +14,26 @@ class Offset
   def format_today(date)
     date.strftime("%d%m") + date.strftime("%Y")[2..]
   end
+
+  def date_modifiers
+    letter_keys = [:a, :b, :c, :d]
+    date_modifiers_array.each_with_index.map do |num, i|
+      [letter_keys[i], num]
+    end.to_h
+  end
+
+  def date_modifiers_array
+    (@date.to_i ** 2).digits.reverse[-4..-1]
+  end
+
+  def make_offsets
+    modifiers = date_modifiers
+    format_keys(@key).each.map do |key_let, key|
+      [key_let, key + modifiers[key_let]]
+    end.to_h
+  end
+
+  def self.offsets(key, date)
+    Offset.new(key, date).make_offsets
+  end
 end
