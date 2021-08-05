5bdd0f9c2228c1e6c07866219a4e947dc5cb813e
Tanner Dale <tanner@tannerdale.com>
2021-08-05 14:14:00 -0500

    Refactor method arrangement

--- lib/key_maker.rb
+++ lib/key_maker.rb
@@ -1,13 +1,15 @@
 module KeyMaker
-  def new_key
-    generate_key
+  def format_keys(key=make_key_string)
+    key.chars.each_cons(2).map do |key|
+      key.join
+    end
   end
 
-  def generate_key
-    format_number(rand(0..99999))
+  def generate_number
+    rand(0..99999)
   end
 
-  def formatt_number(number)
-    number.to_s.rjust(5, '0')
+  def make_key_string
+    generate_number.to_s.rjust(5, '0')
   end
 end
