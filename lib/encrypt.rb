d9a154341d19a5258bbd0eaa2aaab49248b8a64c
Tanner Dale <tanner@tannerdale.com>
2021-08-06 11:58:07 -0500

    Add sub-classes that call their respetive methods from enigma

---
+++ lib/encrypt.rb
@@ -0,0 +1,20 @@
+require './lib/file_worker'
+require './lib/enigma'
+
+ARGV == ['message.txt', 'encrytped.txt']
+ARGV[0] == 'message.txt'
+ARGV[1] == 'encrytped.txt'
+
+class Encryptor < FileWorker
+  def initialize
+    super(ARGV[0], ARGV[1])
+    @method = :encryption
+    @result = encrypted
+  end
+
+  def encrypted
+    Enigma.new.encrypt(read_file(@read_file))
+  end
+end
+
+puts Encryptor.new.output
